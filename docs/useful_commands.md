b3fd8a2adf250e1d91d28f4cbf18ee3aa663e199
mzembron <mateuszzembron@gmail.com>
2023-10-03 00:43:07 +0200

    New world with properly working simulation of tb3, new simulation is based on repo: https://github.com/arshadlab/turtlebot3_multi_robot

--- docs/useful_commands.md
+++ docs/useful_commands.md
@@ -6,3 +6,9 @@ Simple launch from nav2 lib:
 Keyboard teleopearation of one of the robots:
 
 `ros2 run teleop_twist_keyboard teleop_twist_keyboard /cmd_vel:=/robot1/cmd_vel`
+
+SLAM for map scanning:
+`ros2 launch nav2_bringup tb3_simulation_launch.py slam:=True world:=<full-path-to-world>`
+
+Saving map to file
+`ros2 run nav2_map_server map_saver_cli -f <file-name>`
--- launch/nav2_bringup/bringup_launch.py
+++ launch/nav2_bringup/bringup_launch.py
@@ -23,27 +23,29 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
-from launch_ros.actions import PushROSNamespace
-from launch_ros.descriptions import ParameterFile
-from nav2_common.launch import RewrittenYaml, ReplaceString
+from launch_ros.actions import PushRosNamespace
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
-    launch_dir = os.path.join(bringup_dir, 'launch')
+    bringup_dir = get_package_share_directory('multi_robot_simulation')
+    launch_dir = os.path.join(bringup_dir, 'launch', 'nav2_bringup')
 
     # Create the launch configuration variables
     namespace = LaunchConfiguration('namespace')
     use_namespace = LaunchConfiguration('use_namespace')
     slam = LaunchConfiguration('slam')
     map_yaml_file = LaunchConfiguration('map')
+    map_server=LaunchConfiguration('map_server')
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
     autostart = LaunchConfiguration('autostart')
     use_composition = LaunchConfiguration('use_composition')
     use_respawn = LaunchConfiguration('use_respawn')
     log_level = LaunchConfiguration('log_level')
+
+
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
     # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
@@ -54,25 +56,17 @@ def generate_launch_description():
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static')]
 
-    # Only it applys when `use_namespace` is True.
-    # '<robot_namespace>' keyword shall be replaced by 'namespace' launch argument
-    # in config file 'nav2_multirobot_params.yaml' as a default & example.
-    # User defined config file should contain '<robot_namespace>' keyword for the replacements.
-    params_file = ReplaceString(
-        source_file=params_file,
-        replacements={'<robot_namespace>': ('/', namespace)},
-        condition=IfCondition(use_namespace))
-        # condition=False)
-
-
-    configured_params = ParameterFile(
-        RewrittenYaml(
-            source_file=params_file,
-            root_key=namespace,
-            param_rewrites={},
-            convert_types=True),
-        allow_substs=True)
-
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'yaml_filename': map_yaml_file}
+    
+    configured_params = RewrittenYaml(
+        source_file= params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+    
     stdout_linebuf_envvar = SetEnvironmentVariable(
         'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
 
@@ -93,8 +87,12 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
-        default_value='',
         description='Full path to map yaml file to load')
+
+    declare_map_server_cmd = DeclareLaunchArgument(
+            'map_server',
+            default_value='False',
+            description='Whether run a map server per nav2 stack')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -111,7 +109,7 @@ def generate_launch_description():
         description='Automatically startup the nav2 stack')
 
     declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition', default_value='True',
+        'use_composition', default_value='False',
         description='Whether to use composed bringup')
 
     declare_use_respawn_cmd = DeclareLaunchArgument(
@@ -121,10 +119,11 @@ def generate_launch_description():
     declare_log_level_cmd = DeclareLaunchArgument(
         'log_level', default_value='info',
         description='log level')
-
+    
+   
     # Specify the actions
     bringup_cmd_group = GroupAction([
-        PushROSNamespace(
+        PushRosNamespace(
             condition=IfCondition(use_namespace),
             namespace=namespace),
 
@@ -146,13 +145,14 @@ def generate_launch_description():
                               'autostart': autostart,
                               'use_respawn': use_respawn,
                               'params_file': params_file}.items()),
-
+       
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(launch_dir,
                                                        'localization_launch.py')),
             condition=IfCondition(PythonExpression(['not ', slam])),
             launch_arguments={'namespace': namespace,
                               'map': map_yaml_file,
+                              'map_server': map_server,
                               'use_sim_time': use_sim_time,
                               'autostart': autostart,
                               'params_file': params_file,
@@ -174,9 +174,11 @@ def generate_launch_description():
     # Create the launch description and populate
     ld = LaunchDescription()
 
+
+
     # Set environment variables
     ld.add_action(stdout_linebuf_envvar)
-
+    
     # Declare the launch options
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_namespace_cmd)
